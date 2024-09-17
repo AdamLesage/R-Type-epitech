6b5bb6417f97202c470b550a06e4404077ab6602
AdamLesage <adamles44@gmail.com>
2024-09-17 11:15:57 +0200

    feat(Project installation): create CMake file and script to execute everything in one command start_project.sh script will build project with CMake and move binaries to root repositoy

---
+++ src/server/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type-epitech
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello R-Type" << std::endl;
+    return 0;
+}
---
+++ start_project.sh
@@ -0,0 +1,38 @@
+# This file is used to start the project such as:
+# Compile the project with Cmake, move binaries to the root directory and delete build/ directory
+
+# Check if the build directory exists
+if [ -d "build" ]; then
+    # If the build directory exists, remove it
+    rm -rf build
+fi
+
+# Create a build directory
+mkdir build
+
+# Move to the build directory
+cd build
+
+# Compile the project with Cmake
+cmake ..
+
+# Compile the project with make
+make
+
+# Move the binaries to the root directory if files exist
+if [ -f "r-type_server" ]; then
+    mv r-type_server ..
+fi
+
+if [ -f "r-type_client" ]; then
+    mv r-type_client ..
+fi
+
+# Move back to the root directory
+cd ..
+
+# Remove the build directory
+rm -rf build
+
+# Print a message to the user
+echo "Project compiled successfully"
