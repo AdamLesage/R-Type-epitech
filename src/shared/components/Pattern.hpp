0be3883c0a45648531be665f3e42ada48af7ee86
rchevallier1234 <romain.chevallier@epitech.eu>
2024-09-25 16:47:55 +0200

    feat(Component): add the Pattern component this component will be use for the mouvement of a monster

---
+++ src/shared/components/Pattern.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type-epitech
+** File description:
+** Patern
+*/
+
+#ifndef PATTERN_HPP_
+#define PATTERN_HPP_
+
+typedef struct Pattern_s {
+    /**
+     * @brief PaternCompoent add a patern for an entity
+     *
+     */
+    float amplitude;  // oscillation
+    float frequency;   // Frequency of oscillation
+} Pattern;
+
+#endif
