c4dec4e69e08b00ccf062438fa7858d9ae934110
rchevallier1234 <romain.chevallier@epitech.eu>
2024-09-30 16:54:04 +0200

    feat(Server): add the method sendCreateEnemy on the NetworkSender class this method send the event of a new enemy created

--- src/server/GameLogique/NetworkSender.cpp
+++ src/server/GameLogique/NetworkSender.cpp
@@ -26,9 +26,14 @@ void NetworkSender::sendCreatePlayer(size_t id, float pos_x, float pos_y)
     this->_network->sendToAll(data.data(), data.size());   
 }
 
-void NetworkSender::sendCreateEnemy(size_t type, size_t id, float pos_x, float pos_y)
+void NetworkSender::sendCreateEnemy(char type, size_t id, float pos_x, float pos_y)
 {
-
+    std::array<char, 13> data{};
+    data[0] = type;
+    std::memcpy(&data[1], &id, sizeof(id));
+    std::memcpy(&data[5], &pos_x, sizeof(float));
+    std::memcpy(&data[9], &pos_y, sizeof(float));
+    this->_network->sendToAll(data.data(), data.size()); 
 }
 
 void NetworkSender::sendCreateWall(size_t id, float pos_x, float pos_y, float size_x, float size_y)
