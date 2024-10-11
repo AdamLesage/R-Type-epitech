920eb8322f7e459fbdbbd26fbe002553a20a480f
AdamLesage <adamles44@gmail.com>
2024-10-11 14:54:41 +0200

    feat(Player gameplay): player can press many keys and use them at the same time close #151

--- src/shared/systems/Systems.hpp
+++ src/shared/systems/Systems.hpp
@@ -29,7 +29,9 @@
     #include "../components/Tag.hpp"
     #include "../components/Type.hpp"
     #include "../components/Velocity.hpp"
+    #include "../../client/Mediator/IMediator.hpp"
     #include <iostream>
+    #include <memory>
 
 class Systems {
     public:
@@ -45,8 +47,10 @@ class Systems {
          *
          * @param reg The registry containing the components.
          * @param window The window to get the input from.
+         * @param mediator The mediator to notify the game engine.
+         * @param shootSound The function to play the shoot sound.
          */
-        int control_system(Registry &reg, sf::RenderWindow &window);
+        void control_system(Registry &reg, sf::RenderWindow &window, std::shared_ptr<RType::IMediator> mediator, std::function<void()> shootSound);
 
         /**
          * @brief Draw all entities with a position and a drawable component.
