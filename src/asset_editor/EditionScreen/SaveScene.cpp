53b4ba517266e4ea17c807f58dc52e2ef3811336
AdamLesage <adamles44@gmail.com>
2024-10-22 16:30:07 +0200

    feat(Asset editor): file saving is able to save every entities with their components

--- src/asset_editor/EditionScreen/SaveScene.cpp
+++ src/asset_editor/EditionScreen/SaveScene.cpp
@@ -16,6 +16,7 @@ Edition::SaveScene::SaveScene(std::string scenePath, std::vector<std::shared_ptr
 Edition::SaveScene::~SaveScene()
 {
 }
+
 void Edition::SaveScene::save()
 {
     std::ofstream outFile(_scenePath);
@@ -30,6 +31,14 @@ void Edition::SaveScene::save()
         outFile << "\t\t{" << std::endl;
 
         this->saveEntityCode(outFile, it);
+
+        outFile << "\t\t\tcomponents: {" << std::endl;
+
+        this->saveComponentPosition(outFile, it);
+        this->saveComponentSize(outFile, it);
+        this->saveComponentRotation(outFile, it);
+
+        outFile << "\t\t\t}" << std::endl;
 
         if (it != _assets.back()) {
             outFile << "\t\t}," << std::endl;
@@ -47,4 +56,34 @@ void Edition::SaveScene::save()
 void Edition::SaveScene::saveEntityCode(std::ofstream &outFile, std::shared_ptr<Asset> asset)
 {
     outFile << "\t\t\tcode: " << asset->getEntityCode() << "," << std::endl;
+}
+
+void Edition::SaveScene::saveComponentPosition(std::ofstream &outFile, std::shared_ptr<Asset> asset)
+{
+    try {
+        Position position = asset->getComponent<Position>();
+        outFile << "\t\t\t\tposition: {x: " << position.x << ", y: " << position.y << "}," << std::endl;
+    } catch (const std::exception &e) {
+        return; // No position component
+    }
+}
+
+void Edition::SaveScene::saveComponentSize(std::ofstream &outFile, std::shared_ptr<Asset> asset)
+{
+    try {
+        Size size = asset->getComponent<Size>();
+        outFile << "\t\t\t\tsize: {x: " << size.x << ", y: " << size.y << "}," << std::endl;
+    } catch (const std::exception &e) {
+        return; // No size component
+    }
+}
+
+void Edition::SaveScene::saveComponentRotation(std::ofstream &outFile, std::shared_ptr<Asset> asset)
+{
+    try {
+        Rotation rotation = asset->getComponent<Rotation>();
+        outFile << "\t\t\t\trotation: " << rotation.rotation << "," << std::endl;
+    } catch (const std::exception &e) {
+        return; // No rotation component
+    }
 }
--- src/asset_editor/EditionScreen/SaveScene.hpp
+++ src/asset_editor/EditionScreen/SaveScene.hpp
@@ -46,6 +46,30 @@ namespace Edition {
              */
             void saveEntityCode(std::ofstream &outFile, std::shared_ptr<Asset> asset);
 
+            /**
+             * @brief Write the component position into the file such as "position: {x: 100, y: 100}",
+             * 
+             * @param outFile The file to write into
+             * @param asset The asset to save
+             */
+            void saveComponentPosition(std::ofstream &outFile, std::shared_ptr<Asset> asset);
+
+            /**
+             * @brief Write the component size into the file such as "size: {x: 100, y: 100}",
+             * 
+             * @param outFile The file to write into
+             * @param asset The asset to save
+             */
+            void saveComponentSize(std::ofstream &outFile, std::shared_ptr<Asset> asset);
+
+            /**
+             * @brief Write the component rotation into the file such as "rotation: 0",
+             * 
+             * @param outFile The file to write into
+             * @param asset The asset to save
+             */
+            void saveComponentRotation(std::ofstream &outFile, std::shared_ptr<Asset> asset);
+
             std::string _scenePath;
             std::vector<std::shared_ptr<Asset>> _assets;
             libconfig::Config _cfg;
