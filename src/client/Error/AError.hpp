dd33961162d17f4f6caaecc1266fc837fa887d0f
AdamLesage <adamles44@gmail.com>
2024-10-31 10:05:34 +0100

    docs(Error briefs): add documentation brief for each function of Error classes

--- src/client/Error/AError.hpp
+++ src/client/Error/AError.hpp
@@ -12,16 +12,41 @@
 #include <iostream>
 
 namespace RType {
+    /**
+     * @class AError abstract class for errors
+     * @author Adam Lesage
+     */
     class AError : public IError {
         public:
+            /**
+             * @brief construct a new error instance
+             * @param message String to display while catching the error
+             * @param location Current file or current function of the error thrown
+             * @author Adam Lesage
+             */
             AError(const std::string& message, const std::string& location) {
                 _errorMessage  = message;
                 _errorLocation = location;
             }
+
+            /**
+             * @brief Delete the current instance of the object
+             * @author Adam Lesage
+             */
             virtual ~AError() noexcept = default;
+
+            /**
+             * @brief Show the error message while catching the error
+             * @author Adam Lesage
+             */
             const char* what() const noexcept override {
                 return (_errorMessage.c_str());
             }
+
+            /**
+             * @brief Show the location of the error while catching the error
+             * @author Adam Lesage
+             */
             const char* where() const noexcept override {
                 return (_errorLocation.c_str());
             }
@@ -29,7 +54,7 @@ namespace RType {
         protected:
             std::string _errorMessage;
             std::string _errorLocation;
-    };
+    }; // class AError
 } // namespace RType
 
 #endif /* !AERROR_HPP_ */
--- src/client/Error/IError.hpp
+++ src/client/Error/IError.hpp
@@ -11,14 +11,30 @@
 #include <iostream>
 
 namespace RType {
+    /**
+     * @class IError interface of error class
+     */
     class IError : public std::exception {
         public:
+            /**
+             * @brief Delete IError instance
+             */
             virtual ~IError() noexcept                         = default;
+
+            /**
+             * @brief Show the error message while catching the error
+             * @author Adam Lesage
+             */
             virtual const char* what() const noexcept override = 0;
+
+            /**
+             * @brief Show the location of the error while catching the error
+             * @author Adam Lesage
+             */
             virtual const char* where() const noexcept         = 0;
 
         protected:
-    };
+    }; // class IError
 } // namespace RType
 
 #endif /* !IERROR_HPP_ */
