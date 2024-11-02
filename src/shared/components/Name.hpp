48949fc8558ca607e9205c6c4bbaf49f8ec76e77
Klayni <klayni.milandou@epitech.eu>
2024-11-02 21:44:24 +0100

    feat(Name): Add Name component to name a player

---
+++ src/shared/components/Name.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** rtype [WSL: Ubuntu-22.04]
+** File description:
+** Name
+*/
+
+#ifndef NAME_HPP_
+#define NAME_HPP_
+
+#include "string"
+
+/**
+ * @struct Name_s
+ */
+typedef struct Name_s {
+        /**
+         * @brief Name component, add a name to an entity
+         *
+         */
+        std::string name; // name of the entity (ex: player1, best_player, Xx360_NoScopxX)
+} Name;
+
+#endif /* !NAME_HPP_ */
